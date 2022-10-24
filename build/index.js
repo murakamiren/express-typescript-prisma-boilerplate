294b422a13793065549d52d06ace81774924d264
renmurakami <autoeyy@gmail.com>
2022-10-24 14:26:24 +0900

    build

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var util_1 = require("./util/util");
+console.log("hello");
+console.log(util_1.utilText);
+//# sourceMappingURL=index.js.map
---
+++ build/util/util.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+exports.utilText = void 0;
+exports.utilText = "util";
+//# sourceMappingURL=util.js.map
